e5f22fe09ace3e5db3b79ab41cdb9942a928236f
Fsp30 <depaulacostafilipe@gmail.com>
2025-04-11 18:15:55 -0300

    Retirando componetes do main (1.0.1)

--- app/src/pages/About.tsx
+++ app/src/pages/About.tsx
@@ -1,6 +1,6 @@
 export default function About(){
         return(
-                <div className="p-6">
+                <div className="p-6 ">
                         <h1 className="text-3x1 font-bold text-blue-600">About</h1>
                 </div>
         )
