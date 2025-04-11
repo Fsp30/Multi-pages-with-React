234072222e397243d7582b5bb13f0b4618d31a4b
Fsp30 <depaulacostafilipe@gmail.com>
2025-04-11 18:04:44 -0300

    Implementando as rotas 1.0.0

---
+++ app/src/pages/About.tsx
@@ -0,0 +1,7 @@
+export default function About(){
+        return(
+                <div className="p-6">
+                        <h1 className="text-3x1 font-bold text-blue-600">About</h1>
+                </div>
+        )
+}
---
+++ app/src/pages/Home.tsx
@@ -0,0 +1,7 @@
+export default function Home(){
+        return(
+                <div className="p-6">
+                        <h1 className="text-3x1 font-bold text-blue-600">Home Page</h1>
+                </div>
+        )
+}
