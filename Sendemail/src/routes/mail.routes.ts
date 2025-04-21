7380e2880b8afdf09e173c752275a138e72d3e84
Fsp30 <depaulacostafilipe@gmail.com>
2025-04-21 16:23:00 -0300

    update routes

--- Sendemail/src/routes/mail.routes.ts
+++ Sendemail/src/routes/mail.routes.ts
@@ -1,14 +1,15 @@
 import { Router } from "express"
 import { MailtrapService } from "../services/MailtrapService"
-import { MailController } from "../controllers/MailController"
+import { MailController } from "../controllers/MailController" 
+
+
 
 const router = Router()
 const mailService = new MailtrapService()
 const controller = new MailController(mailService)
 
 router.post("/send", async (req, res) => {
-        await controller.handleSend(req, res);
-      });
-      
+        await controller.handleSend(req, res)
+      })
 
 export default router
