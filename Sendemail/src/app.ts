792f2822c103b07555fd55026bb0639ff9c3c59c
Fsp30 <depaulacostafilipe@gmail.com>
2025-04-21 16:32:52 -0300

    up to app.ts

--- Sendemail/src/app.ts
+++ Sendemail/src/app.ts
@@ -1,1 +1,10 @@
-import nodemailer from 'nodemailer'
+import express from 'express'
+import router from './routes/mail.routes'
+import cors from 'cors'
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use('/send', router)
+
+export default app
