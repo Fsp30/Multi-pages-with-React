56dc173102d1e2d91fdba26af5c7b4908bc3759b
Fsp30 <depaulacostafilipe@gmail.com>
2025-04-21 17:05:16 -0300

    fix: update Interface from test

--- Sendemail/src/interfaces/IMailService.ts
+++ Sendemail/src/interfaces/IMailService.ts
@@ -1,3 +1,9 @@
 export interface IMailService{
         sendMail(to:string, subject:string, body:string): Promise<void>
+        formatEmail(data: { name: string, email: string, message: string }): {
+                to: string
+                subject: string
+                text: string
+        }
+              
 }
