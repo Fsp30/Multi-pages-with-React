88d315d81ca6abf22af7f98c2d0e01233306dba6
Fsp30 <depaulacostafilipe@gmail.com>
2025-04-22 20:17:50 -0300

    Injection contact form

--- app/src/pages/Contact.tsx
+++ app/src/pages/Contact.tsx
@@ -1,7 +1,6 @@
 import axios from 'axios'
 import { useEffect, useState } from 'react'
-import { Mail, UsersRound, AtSign, MailCheck } from 'lucide-react'
-import { motion, AnimatePresence } from "framer-motion";
+import { ContactForm } from '../components/ContactForm';
 
 interface GitUser {
   avatar_url: string
@@ -24,12 +23,6 @@ export default function Contact() {
       })
   }, [])
 
-  const [sent, setSent] = useState(false)
-  const handleSend = (e: React.FormEvent) => {
-    e.preventDefault()
-    setSent(true)
-    setTimeout(() => setSent(false), 3000)
-  }
 
   return (
     <div className="h-screen overflow-y-auto p-6 bg-gray-900 text-white flex flex-col items-center">
@@ -40,63 +33,7 @@ export default function Contact() {
           Me mande uma mensagem ou confira meus projetos e redes sociais abaixo!
         </p>
 
-        <form onSubmit={handleSend} className="w-full max-w-md space-y-4 mb-10">
-          <div className="w-full p-3 gap-2 rounded-lg bg-gray-700 text-white flex ">
-            <UsersRound />
-            <input
-              type="text"
-              placeholder="Nome"
-              className='w-full border-0 focus:ring-0 focus:outline-none bg-gray-700 text-zinc-400 rounded-lg'
-            />
-          </div  >
-          <div className="w-full p-3 gap-2 rounded-lg bg-gray-700 text-white flex ">
-            <AtSign />
-            <input
-              type="email"
-              placeholder="Email"
-              className="w-full border-0 focus:ring-0 focus:outline-none bg-gray-700 text-zinc-400 rounded-lg"
-            />
-          </div>
-          <div className="w-full p-3 gap-2 rounded-lg bg-gray-700 text-white flex ">
-            <Mail />
-            <textarea
-              placeholder="Mensagem"
-              className="w-full border-0 focus:ring-0 focus:outline-none bg-gray-700 text-zinc-400 rounded-lg"
-            />
-
-          </div>
-          <button
-            type="submit"
-            className="w-full h-8 p-3 bg-zinc-500 hover:bg-gray-800 text-white rounded-lg transition relative overflow-hidden flex justify-center items-center"
-          >
-            <AnimatePresence mode="wait">
-              {!sent ? (
-                <motion.span
-                  key="text"
-                  initial={{ y: 0, opacity: 1 }}
-                  animate={{ y: sent ? 20 : 0, opacity: sent ? 0 : 1 }}
-                  exit={{ y: -20, opacity: 0 }}
-                  transition={{ duration: 0.4 }}
-                  className="absolute"
-                >
-                  Enviar Mensagem
-                </motion.span>
-              ) : (
-                <motion.div
-                  key="icon"
-                  initial={{ scale: 0.5, opacity: 0 }}
-                  animate={{ scale: 1, opacity: 1 }}
-                  exit={{ opacity: 0 }}
-                  transition={{ duration: 0.6 }}
-                  className="absolute"
-                >
-                  <MailCheck size={24} />
-                </motion.div>
-              )}
-            </AnimatePresence>
-          </button>
-
-        </form>
+        <ContactForm/>
 
         {gitData ? (
           <div className="bg-gray-800 p-6 rounded-xl shadow-lg flex flex-col items-center mb-10">
